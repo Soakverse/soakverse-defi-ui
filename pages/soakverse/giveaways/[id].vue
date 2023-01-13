8077b2c88037a15746d3f35dfcde1f6cfd8b407f
Yann Beaudoin <mentortechies@gmail.com>
2023-01-13 12:51:34 -0500

    changed route to giveaways and added redirects

---
+++ pages/soakverse/giveaways/[id].vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container-fluid d-flex">
+    <div class="row mx-auto full">
+      <div class="col-12">
+        <nuxt-link to="/soakverse/eggz/raffles" class="btn btn-primary mb-2"> Back </nuxt-link>
+        <div class="text-center mb-4">
+          <EggzRaffle :raffleId="$route.params.id" :privateRaffle="route.query.privateRaffle" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import EggzRaffle from "~~/components/Soakverse/Eggz/EggzRaffle.vue";
+const route = useRoute();
+useHead({
+  title: "Soakverse Eggz Giveaway - Soakverse DeFI Platform",
+});
+</script>
---
+++ pages/soakverse/giveaways/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container-fluid d-flex">
+    <div class="row mx-auto full">
+      <div class="col-12">
+        <nuxt-link to="/soakverse/eggz/staking" class="btn btn-primary mb-2"> Eggz Staking </nuxt-link>
+        <div class="text-center mb-4">
+          <EggzRaffles />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import EggzRaffles from "~~/components/Soakverse/Eggz/EggzRaffles.vue";
+useHead({
+  title: "Soakverse Eggz Giveaways - Soakverse DeFI Platform",
+});
+</script>
