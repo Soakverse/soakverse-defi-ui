4dc6cc4b59a70d6e0ab2ebb01307f38ad7d16f9e
Yann Beaudoin <mentortechies@gmail.com>
2023-01-13 12:58:36 -0500

    changed route to giveaways and added redirects

--- pages/soakverse/eggz/raffles/[id].vue
+++ pages/soakverse/eggz/raffles/[id].vue
@@ -1,13 +1,6 @@
 <template>
   <div class="container-fluid d-flex">
-    <div class="row mx-auto full">
-      <div class="col-12">
-        <nuxt-link to="/soakverse/giveaways" class="btn btn-primary mb-2"> Back </nuxt-link>
-        <div class="text-center mb-4">
-          <EggzRaffle :raffleId="$route.params.id" :privateRaffle="route.query.privateRaffle" />
-        </div>
-      </div>
-    </div>
+    <div class="row mx-auto full"></div>
   </div>
 </template>
 
