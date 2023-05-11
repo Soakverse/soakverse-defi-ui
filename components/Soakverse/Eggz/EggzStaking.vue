19170380152608b4204897d2eb0260e06ec1b505
Yann Beaudoin <mentortechies@gmail.com>
2023-05-11 08:00:37 -0400

    adding unstake all

--- components/Soakverse/Eggz/EggzStaking.vue
+++ components/Soakverse/Eggz/EggzStaking.vue
@@ -7,44 +7,76 @@
       </h5>
       <h5>Pasturage Count: {{ state.stakedAmount }}</h5>
       <div v-if="state.canStake">
-        <a v-if="state.unstakedEggz.length > 0" @click="stakeAllNfts()" class="btn btn-success btn-sm mx-1"
+        <a
+          v-if="state.unstakedEggz.length > 0"
+          @click="stakeAllNfts()"
+          class="btn btn-success btn-sm mx-1"
           >Stake All</a
         >
-        <!--<a v-if="state.stakedEggz.length > 0" @click="unstakeAllNfts()" class="btn btn-danger btn-sm mx-1"
+        <a
+          v-if="state.stakedEggz.length > 0"
+          @click="unstakeAllNfts()"
+          class="btn btn-danger btn-sm mx-1"
           >Unstake All</a
-        >-->
+        >
       </div>
       <hr />
-      <div class="row" v-if="connectedWallet && chainInformation.chainId == '1'">
+      <div
+        class="row"
+        v-if="connectedWallet && chainInformation.chainId == '1'"
+      >
         <div class="col-12">
           <div class="row">
-            <div v-if="state.highestOwnedStache" class="col-12 col-sm-6 col-md-3 col-lg-2 py-3">
+            <div
+              v-if="state.highestOwnedStache"
+              class="col-12 col-sm-6 col-md-3 col-lg-2 py-3"
+            >
               <h6>{{ state.highestOwnedStache.nft.metadata.name }}</h6>
-              <a @click="comingSoon(1)" class="btn btn-success btn-sm mb-1">Change</a>
+              <a @click="comingSoon(1)" class="btn btn-success btn-sm mb-1"
+                >Change</a
+              >
               <img
                 :src="state.highestOwnedStache.nft.media[0].gateway"
                 class="img-responsive w-100"
                 style="border: 4px solid #e1b77e; border-radius: 4px"
               />
             </div>
-            <div v-for="eggz in state.ownedEggz" :key="eggz.id" class="col-12 col-sm-6 col-md-3 col-lg-2 py-3">
+            <div
+              v-for="eggz in state.ownedEggz"
+              :key="eggz.id"
+              class="col-12 col-sm-6 col-md-3 col-lg-2 py-3"
+            >
               <h6>{{ eggz.name }}</h6>
-              <a @click="comingSoon(2)" class="btn btn-success btn-sm mb-1">Add a Wizh</a>
+              <a @click="comingSoon(2)" class="btn btn-success btn-sm mb-1"
+                >Add a Wizh</a
+              >
               <img :src="eggz.webImage" class="img-responsive w-100" />
               <div v-if="state.canStake">
                 <div v-if="state.stakedEggz.includes(eggz.tokenId)">
-                  <a @click="unstakeNft(eggz.tokenId)" class="btn btn-danger btn-sm mt-1">Release</a>
+                  <a
+                    @click="unstakeNft(eggz.tokenId)"
+                    class="btn btn-danger btn-sm mt-1"
+                    >Release</a
+                  >
                   <p class="green-text mt-1">
-                    Training for {{ Math.trunc(formatDaysSinceDate(eggz.stakedAt)) }} day(s)
+                    Training for
+                    {{ Math.trunc(formatDaysSinceDate(eggz.stakedAt)) }} day(s)
                   </p>
                 </div>
-                <a v-else @click="stakeNft(eggz.tokenId)" class="btn btn-success btn-sm">Stake</a>
+                <a
+                  v-else
+                  @click="stakeNft(eggz.tokenId)"
+                  class="btn btn-success btn-sm"
+                  >Stake</a
+                >
               </div>
               <div v-else>
                 <p class="red-text">Staking not activated yet</p>
               </div>
             </div>
-            <div v-if="state.ownedEggz.length == 0"><h6 class="my-3">You own no Eggz.</h6></div>
+            <div v-if="state.ownedEggz.length == 0">
+              <h6 class="my-3">You own no Eggz.</h6>
+            </div>
           </div>
         </div>
       </div>
@@ -56,10 +88,16 @@
 </template>
 
 <script setup>
-import { showLoader, hideLoader, filterArrayOfObjects, formatDaysSinceDate } from "~~/utils/helpers";
+import {
+  showLoader,
+  hideLoader,
+  filterArrayOfObjects,
+  formatDaysSinceDate,
+} from "~~/utils/helpers";
 import eggzABI from "@/assets/abi/EggzABI";
 
-const { setNetwork, addNetwork, addAsset, chainInformation, connectedWallet } = useWeb3WalletState();
+const { setNetwork, addNetwork, addAsset, chainInformation, connectedWallet } =
+  useWeb3WalletState();
 
 const config = useRuntimeConfig();
 
@@ -70,16 +108,21 @@ const eggzContractAddress = "0x10b366bbf2304b52806b1c9881fc259bd9018d43";
 const stachesContractAddress = "0x2019f1aa40528e632b4add3b8bcbc435dbf86404";
 
 const nftLevels = [
-  5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 1, 4, 1, 2, 2, 1, 3, 3, 2, 1, 1, 5, 1, 2, 4, 1, 1, 1, 3, 2, 3, 5, 1, 3, 1, 2, 3, 1, 1,
-  1, 4, 3, 3, 4, 1, 1, 1, 4, 1, 3, 1, 1, 1, 2, 2, 4, 4, 2, 1, 5, 2, 1, 4, 1, 2, 5, 2, 3, 2, 1, 2, 5, 1, 4, 4, 1, 2, 3,
-  2, 3, 2, 2, 2, 3, 1, 1, 5, 4, 3, 2, 2, 2, 2, 1, 1, 1, 3, 1, 2, 3, 2, 1, 1, 3, 3, 4, 1, 1, 1, 5, 3, 3, 1, 1, 1, 2, 2,
-  3, 2, 2, 1, 2, 1, 3, 1, 1, 4, 2, 3, 2, 2, 3, 5, 3, 2, 1, 1, 1, 4, 1, 3, 2, 3, 5, 1, 3, 1, 4, 3, 3, 2, 1, 3, 3, 1, 1,
-  1, 1, 1, 2, 3, 3, 1, 1, 1, 1, 2, 3, 2, 5, 1, 1, 2, 3, 1, 1, 1, 1, 2, 5, 2, 1, 5, 1, 1, 1, 5, 3, 2, 3, 2, 2, 3, 2, 3,
-  2, 2, 3, 1, 2, 1, 1, 4, 1, 1, 1, 3, 4, 4, 3, 2, 1, 1, 1, 3, 1, 4, 1, 3, 4, 1, 4, 1, 1, 3, 1, 2, 1, 5, 1, 1, 4, 2, 2,
-  3, 2, 5, 3, 2, 5, 5, 1, 2, 2, 1, 1, 2, 2, 3, 3, 2, 2, 2, 4, 3, 3, 1, 2, 1, 1, 2, 2, 1, 3, 4, 3, 1, 2, 2, 2, 2, 2, 1,
-  1, 1, 4, 1, 4, 1, 4, 1, 1, 1, 2, 3, 4, 4, 3, 1, 2, 3, 1, 1, 3, 1, 1, 4, 1, 3, 2, 2, 2, 2, 2, 3, 2, 2, 3, 1, 1, 1, 1,
-  1, 1, 2, 2, 2, 1, 2, 2, 1, 2, 1, 2, 5, 2, 1, 1, 5, 1, 2, 2, 2, 5, 1, 3, 3, 2, 1, 2, 3, 3, 2, 2, 3, 3, 1, 3, 2, 1, 4,
-  3, 2, 4, 2, 1, 2, 3, 2, 1, 3, 3, 4, 3, 1,
+  5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 1, 4, 1, 2, 2, 1, 3, 3, 2, 1, 1, 5, 1, 2, 4, 1,
+  1, 1, 3, 2, 3, 5, 1, 3, 1, 2, 3, 1, 1, 1, 4, 3, 3, 4, 1, 1, 1, 4, 1, 3, 1, 1,
+  1, 2, 2, 4, 4, 2, 1, 5, 2, 1, 4, 1, 2, 5, 2, 3, 2, 1, 2, 5, 1, 4, 4, 1, 2, 3,
+  2, 3, 2, 2, 2, 3, 1, 1, 5, 4, 3, 2, 2, 2, 2, 1, 1, 1, 3, 1, 2, 3, 2, 1, 1, 3,
+  3, 4, 1, 1, 1, 5, 3, 3, 1, 1, 1, 2, 2, 3, 2, 2, 1, 2, 1, 3, 1, 1, 4, 2, 3, 2,
+  2, 3, 5, 3, 2, 1, 1, 1, 4, 1, 3, 2, 3, 5, 1, 3, 1, 4, 3, 3, 2, 1, 3, 3, 1, 1,
+  1, 1, 1, 2, 3, 3, 1, 1, 1, 1, 2, 3, 2, 5, 1, 1, 2, 3, 1, 1, 1, 1, 2, 5, 2, 1,
+  5, 1, 1, 1, 5, 3, 2, 3, 2, 2, 3, 2, 3, 2, 2, 3, 1, 2, 1, 1, 4, 1, 1, 1, 3, 4,
+  4, 3, 2, 1, 1, 1, 3, 1, 4, 1, 3, 4, 1, 4, 1, 1, 3, 1, 2, 1, 5, 1, 1, 4, 2, 2,
+  3, 2, 5, 3, 2, 5, 5, 1, 2, 2, 1, 1, 2, 2, 3, 3, 2, 2, 2, 4, 3, 3, 1, 2, 1, 1,
+  2, 2, 1, 3, 4, 3, 1, 2, 2, 2, 2, 2, 1, 1, 1, 4, 1, 4, 1, 4, 1, 1, 1, 2, 3, 4,
+  4, 3, 1, 2, 3, 1, 1, 3, 1, 1, 4, 1, 3, 2, 2, 2, 2, 2, 3, 2, 2, 3, 1, 1, 1, 1,
+  1, 1, 2, 2, 2, 1, 2, 2, 1, 2, 1, 2, 5, 2, 1, 1, 5, 1, 2, 2, 2, 5, 1, 3, 3, 2,
+  1, 2, 3, 3, 2, 2, 3, 3, 1, 3, 2, 1, 4, 3, 2, 4, 2, 1, 2, 3, 2, 1, 3, 3, 4, 3,
+  1,
 ];
 
 const decimals = 18;
@@ -99,7 +142,10 @@ onMounted(async () => {
   try {
     const currentChainId = await $web3.eth.net.getId();
     if (process.client && connectedWallet && currentChainId == "1") {
-      eggzContract = await new $web3.eth.Contract(eggzABI.abi, eggzContractAddress);
+      eggzContract = await new $web3.eth.Contract(
+        eggzABI.abi,
+        eggzContractAddress
+      );
       getEcosystemBalance();
     }
   } catch (e) {
@@ -110,7 +156,10 @@ onMounted(async () => {
 watch(connectedWallet, async () => {
   const currentChainId = await $web3.eth.net.getId();
   if (process.client && connectedWallet && currentChainId == "1") {
-    eggzContract = await new $web3.eth.Contract(eggzABI.abi, eggzContractAddress);
+    eggzContract = await new $web3.eth.Contract(
+      eggzABI.abi,
+      eggzContractAddress
+    );
     getEcosystemBalance();
   }
 });
@@ -123,7 +172,9 @@ async function getEcosystemBalance() {
     state.ownedStaches = [];
     state.ownedEggz = [];
     state.highestOwnedStache = null;
-    state.stakedAmount = (await $fetch(`${config.apiUrl}/nft/stats/eggz`)).staked;
+    state.stakedAmount = (
+      await $fetch(`${config.apiUrl}/nft/stats/eggz`)
+    ).staked;
 
     const accounts = await $web3.eth.getAccounts();
 
@@ -144,7 +195,9 @@ async function getEcosystemBalance() {
         attributes: nft.attributes,
       });
 
-      nft.staked ? state.stakedEggz.push(nft.tokenId) : state.unstakedEggz.push(nft.tokenId);
+      nft.staked
+        ? state.stakedEggz.push(nft.tokenId)
+        : state.unstakedEggz.push(nft.tokenId);
     }
 
     const baseURL = `https://eth-mainnet.g.alchemy.com/v2/${config.alchemyApiKey}`;
@@ -201,17 +254,23 @@ async function stakeNft(tokenId) {
 
       const gasPrice = await $web3.eth.getGasPrice();
 
-      const adjustedGasPrice = new ethUtils.BN(gasPrice).add(new ethUtils.BN(190000000000)).toString();
+      const adjustedGasPrice = new ethUtils.BN(gasPrice)
+        .add(new ethUtils.BN(190000000000))
+        .toString();
       const gasLimit = await eggzContract.methods.stake(tokenId).estimateGas({
         from: account,
         gasPrice: adjustedGasPrice,
       });
 
-      const stakingTransaction = await eggzContract.methods.stake(tokenId).send({ from: account, gasLimit: gasLimit });
+      const stakingTransaction = await eggzContract.methods
+        .stake(tokenId)
+        .send({ from: account, gasLimit: gasLimit });
 
       if (stakingTransaction.status) {
         state.stakedEggz.push(tokenId);
-        state.unstakedEggz = state.unstakedEggz.filter((item) => item !== tokenId);
+        state.unstakedEggz = state.unstakedEggz.filter(
+          (item) => item !== tokenId
+        );
         hideLoader();
         $swal.fire({
           title: "Success",
@@ -262,7 +321,9 @@ async function unstakeNft(tokenId) {
 
       const gasPrice = await $web3.eth.getGasPrice();
 
-      const adjustedGasPrice = new ethUtils.BN(gasPrice).add(new ethUtils.BN(10000000000)).toString();
+      const adjustedGasPrice = new ethUtils.BN(gasPrice)
+        .add(new ethUtils.BN(10000000000))
+        .toString();
       const gasLimit = await eggzContract.methods.unstake(tokenId).estimateGas({
         from: account,
         gasPrice: adjustedGasPrice,
@@ -325,11 +386,15 @@ async function stakeAllNfts() {
 
       const gasPrice = await $web3.eth.getGasPrice();
 
-      const adjustedGasPrice = new ethUtils.BN(gasPrice).add(new ethUtils.BN(10000000000)).toString();
-      const gasLimit = await eggzContract.methods.setTokensStakeStatus(state.unstakedEggz, true).estimateGas({
-        from: account,
-        gasPrice: adjustedGasPrice,
-      });
+      const adjustedGasPrice = new ethUtils.BN(gasPrice)
+        .add(new ethUtils.BN(10000000000))
+        .toString();
+      const gasLimit = await eggzContract.methods
+        .setTokensStakeStatus(state.unstakedEggz, true)
+        .estimateGas({
+          from: account,
+          gasPrice: adjustedGasPrice,
+        });
 
       const stakingTransaction = await eggzContract.methods
         .setTokensStakeStatus(state.unstakedEggz, true)
@@ -338,6 +403,73 @@ async function stakeAllNfts() {
       if (stakingTransaction.status) {
         state.stakedEggz = state.stakedEggz.concat(currentlyUnstakedEggz);
         state.unstakedEggz = [];
+        hideLoader();
+        $swal.fire({
+          title: "Success",
+          text: "Staking successful",
+          icon: "success",
+          buttonsStyling: false,
+          customClass: {
+            confirmButton: "btn btn-success btn-fill",
+          },
+        });
+      }
+    }
+  } catch (error) {
+    var strippedError = error.message.split("{");
+    hideLoader();
+    $swal.fire({
+      title: "Error",
+      text: strippedError[0],
+      icon: "error",
+      buttonsStyling: false,
+      customClass: {
+        confirmButton: "btn btn-danger btn-fill",
+      },
+    });
+  }
+}
+
+async function unstakeAllNfts() {
+  const currentlyStakedEggz = state.stakedEggz;
+  if (!(currentlyStakedEggz.length > 0)) {
+    $swal.fire({
+      title: "Error",
+      text: "You don't have Eggz to stake",
+      icon: "error",
+      buttonsStyling: false,
+      customClass: {
+        confirmButton: "btn btn-danger btn-fill",
+      },
+    });
+    return;
+  }
+  try {
+    const accounts = await $web3.eth.getAccounts();
+    if (accounts.length > 0) {
+      showLoader();
+      const account = accounts[0];
+      const ethUtils = $web3.utils;
+
+      const gasPrice = await $web3.eth.getGasPrice();
+
+      const adjustedGasPrice = new ethUtils.BN(gasPrice)
+        .add(new ethUtils.BN(10000000000))
+        .toString();
+      const gasLimit = await eggzContract.methods
+        .setTokensStakeStatus(state.stakedEggz, false)
+        .estimateGas({
+          from: account,
+          gasPrice: adjustedGasPrice,
+        });
+
+      const stakingTransaction = await eggzContract.methods
+        .setTokensStakeStatus(state.stakedEggz, false)
+        .send({ from: account, gasLimit: gasLimit });
+
+      if (stakingTransaction.status) {
+        state.unstakedEggz = state.unstakedEggz.concat(currentlyStakedEggz);
+        state.stakedEggz = [];
         hideLoader();
         $swal.fire({
           title: "Success",
