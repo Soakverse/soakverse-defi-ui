40105e5780650edd89731484b57236171608c921
Yann Beaudoin <mentortechies@gmail.com>
2024-02-18 10:13:49 -0500

    first version of the store system for both phaser and dapp

--- game/scenes/SW_BootScene.ts
+++ game/scenes/SW_BootScene.ts
@@ -1,39 +1,39 @@
-import { SW_CST } from '~/game/SW_CST';
-import SW_BaseScene from '~/game/scenes/SW_BaseScene';
-import SW_GameScene from '~/game/scenes/SW_GameScene';
-import SW_GameUIScene from '~/game/scenes/SW_GameUIScene';
+import { SW_CST } from "~/game/SW_CST";
+import SW_BaseScene from "~/game/scenes/SW_BaseScene";
+import SW_GameScene from "~/game/scenes/SW_GameScene";
+import SW_GameUIScene from "~/game/scenes/SW_GameUIScene";
 
-import thudMp3 from '@/game/assets/thud.mp3';
-import thudOgg from '@/game/assets/thud.ogg';
+import thudMp3 from "@/game/assets/thud.mp3";
+import thudOgg from "@/game/assets/thud.ogg";
 
-import objectCounterBackground from '@/game/assets/inventory/objectCounterBackground.png';
-import objectCounterMinusButton from '@/game/assets/inventory/objectCounterMinusButton.png';
-import objectCounterPlusButton from '@/game/assets/inventory/objectCounterPlusButton.png';
-import objectCounterMoveButton from '@/game/assets/inventory/objectCounterMoveButton.png';
+import objectCounterBackground from "@/game/assets/inventory/objectCounterBackground.png";
+import objectCounterMinusButton from "@/game/assets/inventory/objectCounterMinusButton.png";
+import objectCounterPlusButton from "@/game/assets/inventory/objectCounterPlusButton.png";
+import objectCounterMoveButton from "@/game/assets/inventory/objectCounterMoveButton.png";
 
-import inventorySlider from '@/game/assets/inventory/inventorySlider.png';
-import inventorySliderLine from '@/game/assets/inventory/inventorySliderLine.png';
-import inventorySlot from '@/game/assets/inventory/inventorySlot.png';
-import inventoryTableBackground from '@/game/assets/inventory/inventoryTableBackground.png';
-import inventoryWidgetBackground from '@/game/assets/inventory/inventoryWidgetBackground.png';
-import inventoryItemsImage from '@/game/assets/inventory/items/inventoryItems.png';
-import inventoryItemsJson from '@/game/assets/inventory/items/inventoryItems.json';
+import inventorySlider from "@/game/assets/inventory/inventorySlider.png";
+import inventorySliderLine from "@/game/assets/inventory/inventorySliderLine.png";
+import inventorySlot from "@/game/assets/inventory/inventorySlot.png";
+import inventoryTableBackground from "@/game/assets/inventory/inventoryTableBackground.png";
+import inventoryWidgetBackground from "@/game/assets/inventory/inventoryWidgetBackground.png";
+import inventoryItemsImage from "@/game/assets/inventory/items/inventoryItems.png";
+import inventoryItemsJson from "@/game/assets/inventory/items/inventoryItems.json";
 
-import player from '@/game/assets/characters/player.png';
+import player from "@/game/assets/characters/player.png";
 
 // Dialogue
-import dialogueImage_YB from '@/game/assets/dialogueImage_YB.png';
-import GPADJK_d2_1 from '@/game/assets/GPADJK_d2_1.png';
-import Scribb from '@/game/assets/Scribb.png';
+import dialogueImage_YB from "@/game/assets/dialogueImage_YB.png";
+import GPADJK_d2_1 from "@/game/assets/GPADJK_d2_1.png";
+import Scribb from "@/game/assets/Scribb.png";
 
 // Menus
-import menuHeader from '@/game/assets/ui/menus/menuHeader.png';
-import menuBackground from '@/game/assets/ui/menus/menuBackground.png';
-import menuButtonNormal from '@/game/assets/ui/menus/menuButtonNormal.png';
-import menuButtonPressed from '@/game/assets/ui/menus/menuButtonPressed.png';
+import menuHeader from "@/game/assets/ui/menus/menuHeader.png";
+import menuBackground from "@/game/assets/ui/menus/menuBackground.png";
+import menuButtonNormal from "@/game/assets/ui/menus/menuButtonNormal.png";
+import menuButtonPressed from "@/game/assets/ui/menus/menuButtonPressed.png";
 
 export default class SW_BootScene extends SW_BaseScene {
-  constructor () {
+  constructor() {
     super({ key: SW_CST.SCENES.BOOT });
   }
 
@@ -56,7 +56,10 @@ export default class SW_BootScene extends SW_BaseScene {
     this.load.image("inventoryTableBackground", inventoryTableBackground);
     this.load.image("inventoryWidgetBackground", inventoryWidgetBackground);
 
-    this.load.spritesheet("player", player, { frameWidth: 100, frameHeight: 100 });
+    this.load.spritesheet("player", player, {
+      frameWidth: 100,
+      frameHeight: 100,
+    });
 
     this.load.atlas("inventoryItems", inventoryItemsImage, inventoryItemsJson);
 
@@ -77,15 +80,22 @@ export default class SW_BootScene extends SW_BaseScene {
   ////////////////////////////////////////////////////////////////////////
 
   public create(): void {
-    this.input.setDefaultCursor("url(../game/assets/cursors/cursorNormal.cur), pointer");
+    this.input.setDefaultCursor(
+      "url(/game/assets/cursors/cursorNormal.cur), pointer"
+    );
 
-    const sceneUI = this.scene.add(SW_CST.SCENES.GAME_UI, SW_GameUIScene, true, undefined) as SW_GameUIScene;
+    const sceneUI = this.scene.add(
+      SW_CST.SCENES.GAME_UI,
+      SW_GameUIScene,
+      true,
+      undefined
+    ) as SW_GameUIScene;
 
     this.scene.add(SW_CST.SCENES.GAME, SW_GameScene, true, {
       worldName: "grassWorld",
       //startPosition: new Phaser.Math.Vector2(200, 200)
       previousWorldName: "Starter",
-      spawnPositionName: "Starter"
+      spawnPositionName: "Starter",
     });
     sceneUI.scene.bringToTop();
 
