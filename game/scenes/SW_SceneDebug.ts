09bfabffd0e42460844f636c6758c448cc9b16ea
dany <dhad2301@usherbrooke.ca>
2024-03-03 09:41:43 -0500

    Debug scene now shows FPS

--- game/scenes/SW_SceneDebug.ts
+++ game/scenes/SW_SceneDebug.ts
@@ -3,6 +3,7 @@ import SW_BaseScene from '~/game/scenes/SW_BaseScene';
 
 export default class SW_SceneDebug extends SW_BaseScene {
   protected declare debugTextMapManager: Phaser.GameObjects.Text;
+  protected declare fpsText: Phaser.GameObjects.Text;
 
   constructor() {
     super({ key: SW_CST.SCENES.DEBUG });
@@ -17,16 +18,33 @@ export default class SW_SceneDebug extends SW_BaseScene {
   ////////////////////////////////////////////////////////////////////////
 
   public create(): void {
-    this.debugTextMapManager = this.add.text(24, 24, '', {
+    this.fpsText = this.add.text(this.game.canvas.width - 24, 16, '', {
       fontSize: '18px',
       color: 'white',
       stroke: 'black',
       strokeThickness: 3,
       // backgroundColor '#FFFFFF',
     });
+    this.fpsText.setOrigin(1, 0);
+
+    this.debugTextMapManager = this.add.text(24, 16, '', {
+      fontSize: '18px',
+      color: 'white',
+      stroke: 'black',
+      strokeThickness: 3,
+      // backgroundColor '#FFFFFF',
+    });
+    this.debugTextMapManager.setOrigin(0, 0);
   }
 
   public updateDebugMapManager(text: string): void {
     this.debugTextMapManager.setText(text);
+  }
+
+  // Update
+  ////////////////////////////////////////////////////////////////////////
+
+  public update(): void {
+    this.fpsText.setText(`${this.game.loop.actualFps.toFixed(1)} FPS`);
   }
 }
